d84ebd4da5adb5966f1626bfd8f568fa2ee1d3b9
AlekseiMakonovitskii <alexmakondev@gmail.com>
2024-07-29 23:41:06 +0300

    add chakra ui

---
+++ src/coreComponents/index.ts
@@ -0,0 +1,25 @@
+import {
+  Container,
+  Box,
+  Flex,
+  Spacer,
+  Button,
+  ButtonGroup,
+  Heading,
+  Text,
+  Drawer,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
+
+export {
+  Container,
+  Box,
+  Flex,
+  Spacer,
+  Button,
+  ButtonGroup,
+  Heading,
+  Text,
+  Drawer,
+  DrawerCloseButton,
+};
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes.tsx'
 import i18next from './i18n.ts'
 import { I18nextProvider } from 'react-i18next'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
-      <RouterProvider router={router} /> 
+      <ChakraProvider>
+        <RouterProvider router={router} /> 
+      </ChakraProvider>
     </I18nextProvider>
   </React.StrictMode>,
 )
